92851f855234f456e499c86372cbb83eb927dec2
HAhsCS-4me <67917692+HAhsCS-4me@users.noreply.github.com>
2022-03-01 08:51:28 -0700

    Update sort-analyzer.py

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -46,7 +46,7 @@ def bubbleSort(anArray):
 
 # Selection Sort Function
 def selectionSort(anArray):
-    for i in range(len(anArray) - 1):
+    for i in range(len(anArray) - 2):
         minPosition = i
         for j in range(i + 1, len(anArray)):
             if anArray[j] < anArray[minPosition]:
@@ -65,10 +65,10 @@ def insertionSort(anArray):
 
 
 # Output for Bubble, Selection, Insertion Sort for Random Data
-'''startTime = time.time()
+startTime = time.time()
 selectionSort(randomData)
 endTime = time.time()
-print(f"Sort Random Data: {endTime - startTime} seconds")'''
+print(f"Sort Random Data: {endTime - startTime} seconds")
 
 # Output for Bubble, Selection, Insertion Sort for Reversed Data
 '''startTime = time.time()
@@ -77,10 +77,10 @@ endTime = time.time()
 print(f"Sort Reversed Data: {endTime - startTime} seconds")'''
 
 # Output for Bubble, Selection, Insertion Sort for Nearly Sorted Data
-startTime = time.time()
+'''startTime = time.time()
 selectionSort(nearlySortedData)
 endTime = time.time()
-print(f"Sort Nearly Sorted Data: {endTime - startTime} seconds")
+print(f"Sort Nearly Sorted Data: {endTime - startTime} seconds")'''
 
 # Output for Bubble, Selection, Insertion Sort for Few Unique Data
 '''startTime = time.time()
